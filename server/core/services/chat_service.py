d72b735a57cffdc8b6253892ea13d37a6f9e6ee5
Dan <danii-benitez@hotmail.com>
2024-03-02 18:04:56 -0300

    wip: chatgpt int

--- server/core/services/chat_service.py
+++ server/core/services/chat_service.py
@@ -3,11 +3,14 @@ from core.dependencies import get_repository
 from core.models import Chat, ChatConfig, User, Input
 from core.schemas.chat import ChatCreate, ChatReadDetail, ChatUpdate
 from infrastructure.repositories.chat_repository import ChatRepository
+from infrastructure.repositories.openai_repository import OpenAIRepository
 
 
 class ChatService:
     def __init__(
-        self, chat_repository: ChatRepository = Depends(get_repository(ChatRepository))
+        self, 
+        chat_repository: ChatRepository = Depends(get_repository(ChatRepository)),
+        openai_repository: OpenAIRepository = Depends(get_repository(OpenAIRepository))
     ) -> None:
         self.chat_repository = chat_repository
 
@@ -36,3 +39,17 @@ class ChatService:
         result = self.chat_repository.update_chat(user=user, chat=new_chat, chat_id=chat_id)
 
         return ChatReadDetail(**result.model_dump(), config=chat.config)
+
+
+    def init_chat(self, user: User, chat_id: int, initial_conf: dict[str, str]) -> ChatReadDetail:
+
+        chat = self.get_chat_detail(user=user, chat_id=chat_id)
+        self.update_chat(user=user, chat_id=chat_id, chat=ChatUpdate(config=initial_conf))
+
+        prompt = self.openai_repository.init_chat(config=initial_conf)
+
+        chat.prompts.append(prompt)
+
+        self.update_chat(chat)
+
+        return ChatReadDetail(**chat.model_dump(), config={}, prompts=chat.prompts)
---
+++ server/infrastructure/repositories/openai_repository.py
@@ -0,0 +1,17 @@
+
+
+
+
+from fastapi import HTTPException
+from core.schemas.chat import ChatReadDetail
+
+from infrastructure.repositories.base import BaseRepository
+
+
+class OpenAIRepository(BaseRepository):
+
+    async def create_chat(self, config: dict[str, str]) -> None:
+
+        print(config)
+
+        raise HTTPException(status_code=501, detail="Not Implemented")
