93b921a733b3767ed2178a7589b06011f3c17b9a
Dan <danii-benitez@hotmail.com>
2024-03-05 22:54:56 -0300

    feat: allow any cors

--- server/main.py
+++ server/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from core.models import *
 from core.database import create_db_and_tables
 from api.routers import api_router
 from internal.admin import mount_admin
 
+
+origins = [
+    "*",
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
